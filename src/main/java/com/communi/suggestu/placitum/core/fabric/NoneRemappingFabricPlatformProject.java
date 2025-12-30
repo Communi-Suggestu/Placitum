b3e0f12c576b6ae22871c5d5aab5f94f75eef40e
Marc Hermans <marc.hermans@ldtteam.com>
2025-12-30 16:50:03 +0100

    Use a common published configuration.

--- src/main/java/com/communi/suggestu/placitum/core/fabric/NoneRemappingFabricPlatformProject.java
+++ src/main/java/com/communi/suggestu/placitum/core/fabric/NoneRemappingFabricPlatformProject.java
@@ -2,12 +2,18 @@ package com.communi.suggestu.placitum.core.fabric;
 
 import net.fabricmc.loom.LoomNoRemapGradlePlugin;
 import net.fabricmc.loom.api.LoomGradleExtensionAPI;
+import net.fabricmc.loom.task.RemapJarTask;
 import net.fabricmc.loom.util.Constants;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.attributes.Attribute;
+import org.gradle.api.component.AdhocComponentWithVariants;
 import org.gradle.api.tasks.TaskProvider;
 import org.gradle.api.tasks.bundling.Jar;
+import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
+import java.util.Map;
 
 public abstract class NoneRemappingFabricPlatformProject extends AbstractFabricPlatformProject
 {
@@ -50,9 +56,27 @@ public abstract class NoneRemappingFabricPlatformProject extends AbstractFabricP
     @Override
     protected void includeAndExposeCommonProject(final Project project, final Project commonProject, final TaskProvider<Jar> bundleFmjTask, final String commonProjectName)
     {
-        final var includeConfiguration = project.getConfigurations().maybeCreate(Constants.Configurations.INCLUDE);
-        final var dependency = project.getDependencies().create(bundleFmjTask);
-        includeConfiguration.getDependencies().add(dependency);
+
+        final Configuration include = project.getConfigurations().maybeCreate(Constants.Configurations.INCLUDE);
+        include.getDependencies().add(
+            project.getDependencies().project(Map.of(
+                "path", commonProject.getPath(),
+                "configuration", "bundledElements"
+            ))
+        );
+
+        final Configuration commonBundledElements = commonProject.getConfigurations().maybeCreate("bundledElements");
+        commonProject.getArtifacts().add("bundledElements", bundleFmjTask.flatMap(Jar::getArchiveFile), artifact -> {
+            artifact.builtBy(bundleFmjTask);
+            artifact.setType("jar");
+        });
+
+        commonProject.getComponents().named("java", AdhocComponentWithVariants.class, component -> {
+            component.addVariantsFromConfiguration(commonBundledElements, variant -> {
+                variant.mapToMavenScope("runtime");
+                variant.mapToOptional();
+            });
+        });
     }
 
     @Override
